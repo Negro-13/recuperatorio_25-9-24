2113b0a71d2d36353db81fada2f8d2e1beb78abb
Negro-13 <max.avt8@gmail.com>
2024-09-25 11:27:03 -0300

    fisrt commit

--- src/App.js
+++ src/App.js
@@ -3,8 +3,29 @@ import { useState } from 'react';
 
 function App() {
   const [category, setCategory] = useState("");
+  const [allProducts, setAllProducta] = useState([])
+  const [productByCategory, setProductByCategory] = useState([])
+  const [carts, setCarts] = useState([])
 
   const handleCategoryChange = (event) => setCategory(event.target.value)
+
+  const getAllProducts = () => {
+    fetch('https://fakestoreapi.com/products')
+      .then(response => response.json())
+      .then((data) => setAllProducta(data))
+  }
+  
+  const getProductByCategory = () => {
+    fetch(`https://fakestoreapi.com/products/category/${category}`)
+            .then(response => response.json())
+            .then((data) => setProductByCategory(data))
+  }
+
+  const getCarts = () => {
+    fetch('https://fakestoreapi.com/carts')
+    .then(response => response.json())
+    .then((data) => setCarts(data))
+  }
 
   return (
     <main>
@@ -13,18 +34,18 @@ function App() {
 
       <div>
         <h2>Lista de todos los productos disponibles:</h2>
-        <p className="result-box"></p>
+        <button onClick={getAllProducts}>Mostrar</button>
+        <p className="result-box">{allProducts.map((product) => product.title+ ' su precio es ' + product.price + ' dolares, ')}</p>
       </div>
 
       <div>
         <h2>Obtener productos de una categoría determinada</h2>
-
         <h3>Ingrese una categoría:</h3>
         <input type="text" value={category} onChange={handleCategoryChange} />
-        <button>Enviar</button>
+        <button onClick={getProductByCategory}>Enviar</button>
         
         <h3>Productos de la categoría ingresada:</h3>
-        <p className="result-box"></p>
+        <p className="result-box">{productByCategory.map((product) => product.title + ' su precio es ' + product.price + ' dolares, ')} </p>
       </div>
 
       <div>
@@ -34,9 +55,9 @@ function App() {
 
       <div>
         <h2>Carritos con al menos 2 productos:</h2>
-        <p className="result-box"></p>
+        <button onClick={getCarts}>Mostrar</button>
+        <p className="result-box">{carts.map((cart) => 'El carro ' + cart.id + " tiene " + cart.products.length + ' productos ' + ', ')}</p>
       </div>
-
     </main>
   );
 }
